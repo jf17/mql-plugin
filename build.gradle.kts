234dc9abc664b5487f22408c624d79c988beda19
alex <javafirst17@gmail.com>
2023-11-09 18:48:18 +0300

    Update plugin support version IDEA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.16.0"
 
     // https://plugins.jetbrains.com/docs/intellij/tools-gradle-grammar-kit-plugin.html
     id("org.jetbrains.grammarkit") version "2021.2.2"
 }
 
 group = "ru.jf17"
-version = "0.4.4"
+version = "0.4.5"
 
 repositories {
     mavenCentral()
@@ -16,7 +16,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2")
+    version.set("2023.2.4")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -25,8 +25,8 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
 
     generateLexer{
@@ -38,7 +38,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("223.*")
+        untilBuild.set("232.*")
     }
 
 
--- src/main/java/ru/jf17/mql/MQLIcons.java
+++ src/main/java/ru/jf17/mql/MQLIcons.java
@@ -1,7 +1,9 @@
 package ru.jf17.mql;
+
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
+
 public class MQLIcons {
-    public static final Icon FILE = IconLoader.findIcon("icons/mql.svg");
+    public static final Icon FILE = IconLoader.findIcon("icons/mql.svg", MQLIcons.class);
 }
